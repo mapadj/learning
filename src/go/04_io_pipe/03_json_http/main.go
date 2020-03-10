82a8aaba215c50d60fde4ce7702089f6a69f5920
Marcel Nehrig <marcel.nehrig@gmail.com>
2020-03-10 03:22:43 +0100

    added awesome pipe snippets

---
+++ src/go/04_io_pipe/03_json_http/main.go
@@ -0,0 +1,29 @@
+// Source: https://bit.ly/go-learning-00003
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	pr, pw := io.Pipe()
+
+	go func() {
+		// close the writer, so the reader knows there's no more data
+		defer pw.Close()
+
+		// write json data to the PipeReader through the PipeWriter
+		if err := json.NewEncoder(pw).Encode(&PayLoad{Content: "Hello Pipe!"}); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// JSON from the PipeWriter lands in the PipeReader
+	// ...and we send it off...
+	if _, err := http.Post("http://example.com", "application/json", pr); err != nil {
+		log.Fatal(err)
+	}
+}
