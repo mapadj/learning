2a5122dced76503b11815aab773505c3e4be49d4
Marcel Nehrig <marcel.nehrig@gmail.com>
2020-03-10 03:20:09 +0100

    added more snippets

---
+++ src/go/04_io_pipe/01_tee_reader/main.go
@@ -0,0 +1,50 @@
+// Source: https://bit.ly/go-learning-00003
+
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+func main() {
+
+	pr, pw := io.Pipe()
+
+	// we need to wait for everything to be done
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	// we get some file as input
+	f, err := os.Open("./fruit.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TeeReader gets the data from the file and also writes it to the PipeWriter
+	tr := io.TeeReader(f, pw)
+
+	go func() {
+		defer wg.Done()
+		defer pw.Close()
+
+		// get data from the TeeReader, which feeds the PipeReader through the PipeWriter
+		_, err := http.Post("https://example.com", "text/html", tr)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		// read from the PipeReader to stdout
+		if _, err := io.Copy(os.Stdout, pr); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	wg.Wait()
+}
